48f0c509edb7a475236946d176dbfb214de91d2e
Nithyasri Srivathsan <97502384+Veeraraghavan-S-Nithyasri@users.noreply.github.com>
2023-10-23 11:54:17 +0800

    Second Commit: Functions implementation

--- explore.py
+++ explore.py
@@ -5,7 +5,16 @@ import os.path
 import database
 import sqlparse
 # import re
+#import json
 import os
+import pandas
+
+
+# Notes to self: 
+''' To do for ParseSQL: get_attcol(), get_tabs()
+    To do for Extract: rid_nallowed_str(), attcols()
+    To do for Conversion: query_to_queryTemplate
+'''
 
 # CONNECTION TO DATABASE
 class Conn:
@@ -17,6 +26,21 @@ class Conn:
     # for disconnection
     def disconn(self):
         self.db_conn.close()
+
+    # STATISTICS OF THE DATABASE
+    ''' Note: Learnt this technique of extracting statistical summaries using pandas from ChatGPT 
+        https://chat.openai.com/share/e33e40f4-bc7c-46a0-b38d-42268ef55be3 '''
+    
+    # This function is to be used by GUI part as well as in Query to Query Template Conversion
+    def retreive_stats(query, db_conn):
+        # this function takes the query and database connection object as arguments
+        
+        db = pandas.read_sql_query(query, db_conn)
+
+        stats = db.describe()
+        # save as a csv file
+        stats.to_csv('statistical_summaries.csv')
+        db_conn.close()
 
 # PARSING OF THE SQL QUERY
 class ParseSQL:
@@ -58,6 +82,7 @@ def splitq(self):
 
 def get_attcol(self):
 
+
 # 4. Get the tables
 
 def get_tabs(self):
@@ -71,22 +96,44 @@ class Extract:
     # constructor to initialize table name and table attributes
     def __init__(self, tab):
         self.tab = tab
-        self.attribs = None
+        self.atts = None
 
     def dtype(self):
-        # retreives the datatypes of the attributes of the relation
-    
-    def fkeys(self):
-        # retreives the foreign keys of relation
+        # retreives the datatypes of the attributes of the relation and store as a list/dict
+
+        sql = """SELECT column_name, data_type FROM information_schema.columns WHERE table_name = {tab} AND data_type 
+        IN ('integer', 'double precision', 'real', 'character', 'character varying', 'text', 'date', 'bigint', 'timestamp 
+        with time zone', 'timestamp without time zone')"""
+
+        sql = sql.format(tab = self.tab)
+
+        # Here need to insert code that will actually connect to db and execute the query and do extraction ???
+
     
     def rid_nallowed_strs(self):
-        # to get rid of strings that non-allowed
-    
+        # to get rid of strings that are non-allowed
+        # basically non-alphanumeric cannot be there in the attributes
+
     def att_cols(self):
         # get the columns that are selectable
 
     def write_txt(self):
-        # store into text file
+        # store into text file: 2 cases - dir exists vs doesn't exist
+
+        # does dir exist? yes then write into
+        if os.path.exists('tables'):
+            with open('tables/{}.txt'.format(self.tab), 'w') as filehandle:
+                for x in self.atts:
+                    filehandle.write('{}\n'.format(x))
+        
+        # dir doesn't exist, create, then write into
+        elif not os.path.exists('tables'):
+            # create
+            os.makedirs('tables')
+            # write
+            with open('tables/{}.txt'.format(self.tab), 'w') as filehandle:
+                for x in self.atts:
+                    filehandle.write('{}\n'.format(x))
 
     def get_attribs(self): # function that reads table and returns its attributes
 
@@ -98,7 +145,6 @@ class Extract:
         else:
             self.dtype() # retreives the datatypes of the attributes of the relation
             
-            self.fkeys() # retreives the foreign keys of relation
             self.rid_nallowed_strs() # to get rid of strings that non-allowed
             self.att_cols() # get the columns that are selectable
             
@@ -108,20 +154,57 @@ class Extract:
     
 # CONVERSION OF SQL QUERY
 
-def nested_to_temp(nested_q):
-    # converts a nested query into its template
+# For Brackets '(' and ')' in the queries
+def bracket(str):
+    # Get the string containing the key:value pair bracket_level : the string's content
+    arr = []
+    for x, token in enumerate(str): # token is extracting the brackets or paranthesis
+        
+        # Opening bracket
+        if token == '(':
+            arr.append(x)
+        
+        # Closing Bracket
+        elif token == ')':
+            first = arr.pop()
+            arr_len = len(arr)
+            yield (arr_len, str[first + 1: x]) # returns the value
 
+# Needed in the scenario where there is a subquery
 def temp_to_nested(nested_tok):
     # converts template to nested tokens
+    flag_brackets = False
+    q = nested_tok.value
 
+    for i in list(bracket(q)):
+        if 'select' in i[1].lower():
+            flag_bracket = True
+
+            ans = query_to_queryTemplate(i[1])
+            q = q.replace(i[1], ans)
+    # need to return the flag var as well as q
+    return flag_brackets, q 
+
+# Needed in the scenario where there is a subquery
 def query_to_queryTemplate(q):
     # converts a SQL query into its corresponding template
     
     q_parsed = ParseSQL(q)
     temp = []
 
-def bracket(str):
-    # Get the string containing the key:value pair bracket_level : the string's content
+def nested_to_temp(nested_q):
+    tok = nested_q
+    # converts a nested query into its template
+    for j in list(bracket(nested_q)):
+        
+        c = j[1]
+        if 'select' in c.lower():
+            
+            buf = ParseSQL(c)
+            tok = tok.replace(j[1], query_to_queryTemplate(c))
+    
+    return tok
+
 
 # QUERY EXECUTION PLAN AND OPERATOR TREE
 
