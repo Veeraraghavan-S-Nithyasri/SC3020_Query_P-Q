94a75504d639212366564a0535e64f363f8e9f47
Fabian Tay <104303126+fabbiotaye@users.noreply.github.com>
2023-11-05 20:30:16 +0800

    Initial commit

--- interface.py
+++ interface.py
@@ -1,1 +1,27 @@
 
+'''Streamlit interface test demo'''
+
+import streamlit as st
+
+class dbGUI:
+    def __init__(self):
+        st.title('SC3020 Project 2')
+        st.markdown('''
+                    This interface is designed for visualisation of SQL query execution & exploration.
+                    ''')
+        self.queryInput()
+        
+
+
+    # SQL query input section
+    def queryInput(self):
+        st.header('Query Input')
+        self.inputBox = st.text_area("Key in your SQL query into the box below, and click on the Execute button.", height=100)
+        st.button("Execute")
+
+    
+
+
+
+if __name__ == '__main__':
+    gui = dbGUI()
