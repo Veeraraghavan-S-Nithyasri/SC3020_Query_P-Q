3374e78f14e0cff6884088f2922756bc3cb6b517
Xavier Wong <91479624+WZHYX@users.noreply.github.com>
2023-11-07 10:38:20 +0800

    output section updates

--- interface.py
+++ interface.py
@@ -2,6 +2,7 @@
 '''Streamlit interface test demo'''
 
 import streamlit as st
+import graphviz
 
 class dbGUI:
     def __init__(self):
@@ -10,9 +11,11 @@ class dbGUI:
                     This interface is designed for visualisation of SQL query execution & exploration.
                     ''')
         self.queryInput()
-        self.diskAccessVisual()
-        self.qepDisplay()
-        
+        tab1, tab2 = st.tabs(["Query Execution Plan", "Data Output"])
+        with tab1:
+            self.qepDisplay()
+        with tab2:
+            self.diskAccessVisual()
 
 
     # SQL query input section
@@ -29,7 +32,23 @@ class dbGUI:
 
     # QEP display
     def qepDisplay(self):
-        st.header('Query execution plan')
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("Planning Time: -")
+            st.markdown("Execution Time: -")
+        with col2:
+            graph = graphviz.Graph("QEP viz")
+            graph.node('A', 'Hash join on c=h')
+            graph.node('B', 'Index nested loop join on g=a')
+            graph.node('C', 'W')
+            graph.node('D', 'Sort-merge join on d=f')
+            graph.node('E', 'R')
+            graph.node('F', "Selection on d='Hello' AND e='World'")
+            graph.node('G', 'U')
+            graph.node('H', 'S')
+            graph.edges(['AB', 'AC', 'BD', 'BE', 'DF', 'DG', 'FH'])
+            st.graphviz_chart(graph)
+            
 
 
     
